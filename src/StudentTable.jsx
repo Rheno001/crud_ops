b3052eacdb0d4dc864aaa109b3882a3085b84f4e
Rheno001 <renfredreuben@gmail.com>
2025-03-11 21:54:55 +0100

    update

--- src/StudentTable.jsx
+++ src/StudentTable.jsx
@@ -1,7 +1,10 @@
 export default function StudentTable(){
     return(
-        <div className="container">
-            <h2>Student Records</h2>
+        <div className="container flex flex-col m-auto bg-gray-400 rounded-lg p-4">
+            <h2 className="text-whit">Student Records</h2>
+            <div className="table-container">
+                <a href="#" className="btn btn-add p-4 bg-purple">Add New Student</a>
+            </div>
         </div>
     )
 }
