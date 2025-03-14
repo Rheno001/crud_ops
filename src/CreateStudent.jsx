2ebb0e347df46cbce69da67208aeafb138eb195d
Rheno001 <renfredreuben@gmail.com>
2025-03-14 15:08:13 +0100

    update

--- src/CreateStudent.jsx
+++ src/CreateStudent.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 export default function CreateStudent(){
+    useState("")
     return(
-        <h1>Creating Student</h1>
+        <div className="container flex flex-col items-center justify-center w-[80%] bg-amber-200 rounded-lg p-4 mx-auto">
+            <h2 className="text-black font-bold">Add New Students</h2>
+            <form>
+                <label htmlFor="id">ID</label>
+                <input type="text" id="id" name="id" className="border-2 border-white flex p-2"/>
+
+                <label htmlFor="name">NAME</label>
+                <input type="text" id="name" name="name" className="border-2 border-white flex p-2"/>
+
+                <label htmlFor="place">PLACE</label>
+                <input type="text" id="place" name="place" className="border-2 border-white flex p-2"/>
+
+                <label htmlFor="phone">PHONE</label>
+                <input type="text" id="phone" name="phone" className="border-2 border-white flex p-2"/>
+
+                <div className="flex items-start">
+                <button className="btn-save p-4 rounded-lg bg-green-600 text-white mt-[10px] w-1/2">Save</button>
+                <Link to="/" className="btn-back p-4 rounded-lg bg-red-600 text-white m-[10px] w-1/2 flex justify-center">Back</Link>
+                </div>
+
+                
+            </form>
+        </div>
     )
 }
--- src/StudentTable.jsx
+++ src/StudentTable.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function StudentTable(){
+    const [students, setStudents]=useState("");
+    
+    useEffect(()=>{
+        fetch('http://localhost:8000/students')
+        .then((res)=>res.json())
+        .then((data)=>
+            setStudents(data)).catch((error)=>
+            console.log(error.message))
+    },[])
     return(
-        <div className="container flex flex-col m-auto bg-gray-400 rounded-lg p-4">
-            <h2 className="text-whit">Student Records</h2>
+        <div className="container flex justify-between flex-col m-auto bg-gray-400 rounded-lg p-4">
+            <h2 className="text-white my-[10px] p-4 text-2xl text-center">Student Records</h2>
             <div className="table-container">
-                <a href="#" className="btn btn-add p-4 bg-purple">Add New Student</a>
+                <Link to="/student/create" className="btn-add p-2 bg-green-600 text-white rounded-lg">Add New Student</Link>
+                <table className="my-4 p-[50px] h-[200px] bg-white/30 rounded-lg overflow-hidden">
+                    <thead className="my-[200px]">
+                        <tr className=" bg-green-400 rounded-4xl my-[200px]">
+                            <th className="px-[20px]">ID</th>
+                            <th className="px-[20px]">Name</th>
+                            <th className="px-[20px]">Place</th>
+                            <th className="px-[20px]">Phone</th>
+                            <th className="px-[20px]">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        students && students.map((item)=>(
+                            <tr>
+                            <td className="px-[20px]">{item.id}</td>
+                            <td className="px-[20px]">{item.name}</td>
+                            <td className="px-[20px]">{item.place}</td>
+                            <td className="px-[20px]">{item.phone}</td>
+                            <td className="px-[20px]">
+                                <a href="" className="btn">View</a>
+                                <a href="" className="btn">Edit</a>
+                                <a href="" className="btn btn-del bg-red-600">Delete</a>
+                            </td>
+                        </tr>
+                        )
+                        )
+                       }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
