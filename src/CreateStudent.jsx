8d0ca6ca149f64ad5ccae9b5b0783c8e9a299787
Rheno001 <renfredreuben@gmail.com>
2025-03-17 19:06:44 +0100

    update

--- src/CreateStudent.jsx
+++ src/CreateStudent.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 export default function CreateStudent(){
-    useState("")
+    const[id, setId] = useState("");
+    const[name, setName] = useState("");
+    const[place, setPlace] = useState("");
+    const[phone, setPhone] = useState("");
     return(
         <div className="container flex flex-col items-center justify-center w-[80%] bg-amber-200 rounded-lg p-4 mx-auto">
             <h2 className="text-black font-bold">Add New Students</h2>
             <form>
                 <label htmlFor="id">ID</label>
-                <input type="text" id="id" name="id" className="border-2 border-white flex p-2"/>
+                <input type="text" id="id" name="id" value= {id} onChange={e=>setId(e.target.value)} className="border-2 border-white flex p-2"/>
 
                 <label htmlFor="name">NAME</label>
-                <input type="text" id="name" name="name" className="border-2 border-white flex p-2"/>
+                <input type="text" id="name" name="name" value={name} onChange={e=>setName(e.target.value)} className="border-2 border-white flex p-2"/>
 
                 <label htmlFor="place">PLACE</label>
-                <input type="text" id="place" name="place" className="border-2 border-white flex p-2"/>
+                <input type="text" id="place" name="place" value={place} onChange={e=>setPlace(e.target.value)} className="border-2 border-white flex p-2"/>
 
                 <label htmlFor="phone">PHONE</label>
-                <input type="text" id="phone" name="phone" className="border-2 border-white flex p-2"/>
+                <input type="text" id="phone" name="phone" value={phone} onChange={e=>setPhone(e.target.value)} className="border-2 border-white flex p-2"/>
 
                 <div className="flex items-start">
                 <button className="btn-save p-4 rounded-lg bg-green-600 text-white mt-[10px] w-1/2">Save</button>
